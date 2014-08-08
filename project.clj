fa441cd958557ad0f52264e24f8361c275257c2f
tedfoye <ted.r.foye@gmail.com>
2014-08-07 22:09:44 -0400

    Updated core.asyn and clojurescript version.

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/tedfoye/clojuroids"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [org.clojure/clojurescript "0.0-2280"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
+                 [org.clojure/clojurescript "0.0-2307"]
                  [compojure "1.1.8"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
   :plugins [[lein-ring "0.8.11"]
