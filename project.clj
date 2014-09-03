94364af00c06ff960ab2a69b3a75f68746206dfc
unknown <ted.r.foye@gmail.com>
2014-09-02 22:20:09 -0400

    Updated Clojurescript version to 0.0-2322

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [compojure "1.1.8"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
   :plugins [[lein-ring "0.8.11"]
