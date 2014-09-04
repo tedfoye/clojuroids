bc7576ff072904852e2bf86ae2adb38b9e8be379
unknown <ted.r.foye@gmail.com>
2014-09-03 21:56:04 -0400

    Cleaned up the game loop. Moved level-check to its own namespace.

--- src/cljs/clojuroids/core.cljs
+++ src/cljs/clojuroids/core.cljs
@@ -9,62 +9,47 @@
    [clojuroids.roid :as roid]
    [clojuroids.input :as input]
    [clojuroids.collisions :as collisions]
+   [clojuroids.level :as level]
    [clojuroids.saucer :as saucer])
   (:require-macros
    [cljs.core.async.macros :refer [alt! go-loop]]))
 
+(def frame-rate 33)
 (def init-roid-count 4)
-(def time-to-next-level 100)
 
 (defn timestamp [] (.getTime (js/Date.)))
 
-(defn update-shots [state]
-  (-> state (shot/handle-input) (shot/update)))
+(defn animate-frame [{objects :objects}]
+  (render/animate-frame (reduce concat (vals objects))))
 
-(defn animate-frame [objs]
-  (render/animate-frame (reduce concat (vals objs))))
-
-(defn level-check [state]
-  (let [roids (get-in state [:objects :roids])]
-    (if (= 0 (count roids))
-      (let [timer (:level-timer state)]
-        (cond
-         (= nil timer) (assoc state :level-timer time-to-next-level)
-         (= 0 timer) (-> state
-                         (assoc-in
-                          [:objects :roids]
-                          (roid/create-roids init-roid-count))
-                         (assoc :level-timer nil))
-         (> timer 0) (assoc state :level-timer (dec timer))
-         :else state))
-      state)))
-
-(defn update-state [state c]
+(defn update-state [state]
   (-> state
       (ship/update)
-      (update-shots)
+      (shot/update)
       (roid/update)
       (flames/update)
       (collisions/collisions)
       (explode/update)
-      (level-check)
+      (level/check)
       (saucer/update)))
+
+(defn calc-timeout [start] (- frame-rate (- (timestamp) start))) 
 
 (defn game-loop [init-state]
   (let [input-chan (input/user-input)]
     (go-loop [state init-state start (timestamp)]
       (let [state (merge state{:input (alt! [input-chan] ([v] v) :default nil)})
-            state (update-state state input-chan) 
-            end (- (timestamp) start)]
-        (animate-frame (:objects state))
-        (<! (timeout (- 33 end)))
+            state (update-state state)]
+        (animate-frame state)
+        (<! (timeout (calc-timeout start)))
         (recur state (timestamp))))))
 
 ;; create some asteroids, the ship, a saucer which doesn't do much yet
 ;; and enter the game loop
 (game-loop {:objects {:roids (roid/create-roids init-roid-count)
                       :ship (ship/create)
-                      :saucer (saucer/create 100 100 0)}})
+                      :saucer (saucer/create 100 100 0)}
+            :last-roid-count init-roid-count})
 
 
 
